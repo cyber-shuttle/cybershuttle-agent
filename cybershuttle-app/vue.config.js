abd203d3d356390f92c7827ac6587f1506616f13
Praneeth Chityala <praneethchityala44@gmail.com>
2023-06-21 00:07:20 -0400

    grpc added

--- cybershuttle-app/vue.config.js
+++ cybershuttle-app/vue.config.js
@@ -1,4 +1,34 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+  //   module: {
+  //     rules: [{
+  //             test: /\.(js|vue)$/,
+  //             exclude: /node_modules/,
+  //             use: {
+  //                 loader: 'babel-loader'
+  //             }
+  //         }
+  //     ]
+  // },
+    resolve: {
+      alias: {
+        fs: false,
+        tls: false,
+        net: false,
+        nock: false,
+        // child_process: false,
+        "aws-sdk": false,
+        "mock-aws-s3": false
+      }
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+  },
 })
