de8220189d4759f1a3c254dbba5222acbf83f64b
Praneeth Chityala <praneethchityala44@gmail.com>
2023-06-27 01:49:15 -0400

    dynamic navbar and persisten pinia storage

--- cybershuttle-app/src/main.js
+++ cybershuttle-app/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 
 import { UserServiceGrpc } from './api/grpc/auth';
 
@@ -10,7 +11,10 @@ import router from './router'
 
 const app = createApp(App);
 
+const pinia = createPinia(app);
+pinia.use(piniaPluginPersistedState)
+
 app.provide("UserServiceKey", UserServiceGrpc);
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 app.mount('#app');
